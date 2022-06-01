b8b5102f88423b70901b8e238e76f044d9f85051
Dmitry286 <dmishi@live.spbstu.ru>
2022-06-01 11:34:22 +0300

    Создана модель, вьюшка. Приложение зарегистрировано в админке. Реализован метод get для списка заметок.

--- to_do/models.py
+++ to_do/models.py
@@ -13,17 +13,26 @@ class ToDoNote(models.Model):
     title = models.CharField(max_length=255, verbose_name="Задача")
     task_description = models.TextField(default="", verbose_name="Описание задачи")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    public = models.BooleanField(default=False, verbose_name="Опубликовать") #todo публичные должны видеть все пользователи
     create_at = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
     update_at = models.DateTimeField(auto_now=True, verbose_name="Время обновления")
-    time_point = models.DateTimeField(default=dateTimePoint, blank=True, verbose_name="Время обновления") #todo add calender
+    time_point = models.DateTimeField(default=dateTimePoint, blank=True, verbose_name="Срок выполнения") #todo add calender
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    # status = models.
-
 
 
     def __str__(self):
         return f'Задача: {self.title} № {self.id}'
 
+
     class Meta:
         verbose_name = _("Задача")
         verbose_name_plural = _("Задачи")
+
+
+    class Status(models.IntegerChoices):
+        ACTIVE = 0, _('Активно')
+        POSTPONED = 1, _('Отложено')
+        PERFORMED = 2, _('Выполнено')
+
+
+    status = models.IntegerField(default=Status.ACTIVE, choices=Status.choices, verbose_name="Статус")
--- to_do_api/views.py
+++ to_do_api/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
 from rest_framework.generics import ListAPIView
+from rest_framework.request import Request
+from rest_framework.response import Response
 
 from to_do.models import ToDoNote
 
 from . import serializers
 
-class ToDoNotesAPIView(ListAPIView):
-    queryset = None
-    serializer_class = serializers.
+class ToDoNotesAPIView(APIView):
+    def get(self, request: Request) -> Response:
+        todonotes = ToDoNote.objects.all()
+        serializer = serializers.ToDoNotesAPIViewSerializer(instance=todonotes, many=True)
+
+        return Response(serializer.data)
+
+
+
+
+
+    # queryset = ToDoNote.objects.all()
+    # serializer_class = serializers.ToDoNotesAPIView
+    #
+    # def get_queryset(self):
+    #     queryset = super().get_queryset()
+    #     return queryset.filter(public=True)
+    #
+    # def get(self, request, *args, **kwargs):
+    #     return self.list(request, *args, **kwargs)
